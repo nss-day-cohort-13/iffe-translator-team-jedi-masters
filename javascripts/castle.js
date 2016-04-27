c2fa9658cffab02d049d3d010ff8bc7601167671
Castle Crawford <nathanscottcrawford@gmail.com>
2016-04-27 10:30:06 -0500

    added castle.js

---
+++ javascripts/castle.js
@@ -0,0 +1,15 @@
+var translate = (function(translate) {
+	var newBooty = {
+		merry: "booty",
+		christmas: "booti",
+	    and: "bewti",
+		happy: "buhdi",
+		new: "rockin",
+		year: "everywhere"
+
+	translate.toNewBooty = function() {
+		return newBooty;
+	}
+}
+	return translate;
+}(translate));
