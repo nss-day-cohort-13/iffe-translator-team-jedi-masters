045c97766de1da1de44c28bec0e38978b7ce30ae
Amari Ashwood <fromashestosnow@gmail.com>
2016-04-27 09:31:51 -0500

    final changes and done with klingon js file

---
+++ javascripts/klingon.js
@@ -0,0 +1,21 @@
+var translate = (function (originalTrans) {
+	var klingon = {
+	merry:"Quch",
+	christmas:"jul",
+	and:"je",
+	happy:"Quch",
+	new:"chu\'",
+	year:"DIS"
+	};
+
+
+	originalTrans.toKlingon = function(){
+		return klingon;
+	};
+
+
+
+}(translate)); // iife translate
+
+
+
